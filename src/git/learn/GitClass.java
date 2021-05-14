8d29244761ed2e09754ae2bcad7e99044d1f834d
yanqiong <123619513064@163.com>
2021-05-14 16:23:34 +0800

    add java file to local repository

---
+++ src/git/learn/GitClass.java
@@ -0,0 +1,8 @@
+package git.learn;
+
+public class GitClass {
+    public static void main(String[] args){
+        System.out.println("git is distribute version contrl system");
+        System.out.println("git is free software");
+    }
+}

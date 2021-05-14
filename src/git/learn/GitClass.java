60c0914ecc82c50991016aeda42530d81e9d220c
yanqiong <123619513064@163.com>
2021-05-14 16:26:22 +0800

    version contrl apply idea

--- src/git/learn/GitClass.java
+++ src/git/learn/GitClass.java
@@ -4,5 +4,6 @@ public class GitClass {
     public static void main(String[] args){
         System.out.println("git is distribute version contrl system");
         System.out.println("git is free software");
+        System.out.println("version contrl system apply to idea");
     }
 }
